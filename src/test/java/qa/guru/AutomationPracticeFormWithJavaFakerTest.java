1b3f10f5dba6fd4ab2b108cde4bd0988b53c7ba6
EldarMurzakhanov <eldarmurzahanov@gmail.com>
2023-04-01 01:16:46 +0300

    commit fixed FakerTestData.java commit updated Main Test class

--- src/test/java/qa/guru/AutomationPracticeFormWithJavaFakerTest.java
+++ src/test/java/qa/guru/AutomationPracticeFormWithJavaFakerTest.java
@@ -1,9 +1,7 @@
 package qa.guru;
 
-import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
-import static qa.guru.FakerTestData.*;
 
 public class AutomationPracticeFormWithJavaFakerTest extends TestBase {
 
@@ -12,33 +10,35 @@ public class AutomationPracticeFormWithJavaFakerTest extends TestBase {
     @Test
     void successfulRegistrationTest() {
 
+        FakerTestData alexData = new FakerTestData();
+
         registrationPage.openPage()
                 .removeBanners()
-                .setFirstName(userName)
-                .setLastName(lastName)
-                .setEmail(userEmail)
-                .setGender(userGender)
-                .setPhone(userPhone)
-                .setBirthDate(userBirthDayDay, userBirthDayMonth, userBirthDayYear)
-                .setSubject(userSubject)
-                .setHobby(userHobbies)
-                .setPicture(userPicture)
-                .setAddress(userAddress)
-                .setState(userState)
-                .setCity(userCity)
+                .setFirstName(alexData.userName)
+                .setLastName(alexData.lastName)
+                .setEmail(alexData.userEmail)
+                .setGender(alexData.userGender)
+                .setPhone(alexData.userPhone)
+                .setBirthDate(alexData.userBirthDayDay, alexData.userBirthDayMonth, alexData.userBirthDayYear)
+                .setSubject(alexData.userSubject)
+                .setHobby(alexData.userHobbies)
+                .setPicture(alexData.userPicture)
+                .setAddress(alexData.userAddress)
+                .setState(alexData.userState)
+                .setCity(alexData.userCity)
 
                 .sendSubmit();
 
         registrationPage
-                .verifyResult("Student Name", userName + " " + lastName)
-                .verifyResult("Student Email", userEmail)
-                .verifyResult("Gender", userGender)
-                .verifyResult("Mobile", userPhone)
-                .verifyResult("Date of Birth", userBirthDayDay + " " + userBirthDayMonth + "," + userBirthDayYear)
-                .verifyResult("Subjects", userSubject)
-                .verifyResult("Hobbies", userHobbies)
+                .verifyResult("Student Name", alexData.userName + " " + alexData.lastName)
+                .verifyResult("Student Email", alexData.userEmail)
+                .verifyResult("Gender", alexData.userGender)
+                .verifyResult("Mobile", alexData.userPhone)
+                .verifyResult("Date of Birth", alexData.userBirthDayDay + " " + alexData.userBirthDayMonth + "," + alexData.userBirthDayYear)
+                .verifyResult("Subjects", alexData.userSubject)
+                .verifyResult("Hobbies", alexData.userHobbies)
                 .verifyResult("Picture", "no-homo-sapiens.png")
-                .verifyResult("Address", userAddress)
-                .verifyResult("State and City", userState + " " + userCity);
+                .verifyResult("Address", alexData.userAddress)
+                .verifyResult("State and City", alexData.userState + " " + alexData.userCity);
     }
 }
--- src/test/java/qa/guru/FakerTestData.java
+++ src/test/java/qa/guru/FakerTestData.java
@@ -3,34 +3,45 @@ package qa.guru;
 import com.github.javafaker.Faker;
 
 public class FakerTestData {
-    void fillFormTest() {}
-    public static Faker faker = new Faker();
-    static String userName = faker.name().firstName();
-    static String lastName = faker.name().lastName();
-    static String userEmail = faker.internet().emailAddress();
-    static String userGender = faker.demographic().sex();
-    static String userPhone = "8" + faker.phoneNumber().subscriberNumber(9);
-    static String userBirthDayDay = String.valueOf(faker.number().numberBetween(1, 28));
-    static String userBirthDayMonth = faker.options().option("January", "February",
-                    "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-    static String userBirthDayYear = String.valueOf((faker.number().numberBetween(1950, 2000)));
-    public static String userSubject = faker.options().option("Accounting", "Maths", "Arts", "English", "Physics", "Chemistry",
-                    "Computer Science", "Economics", "Social Studies", "History", "Civics", "Commerce", "Hindi", "Biology");
-    public static String userHobbies = faker.options().option("Reading", "Sports", "Music");
-    static String userPicture = "no-homo-sapiens.png";
-    static String userAddress = faker.address().fullAddress();
-    static String userState = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
-
-    static String city = new String();
+    public FakerTestData (){
+        userName = faker.name().firstName();
+        lastName = faker.name().lastName();
+        userEmail = faker.internet().emailAddress();
+        userGender = faker.demographic().sex();
+        userPhone = "8" + faker.phoneNumber().subscriberNumber(9);
+        userBirthDayDay = String.valueOf(faker.number().numberBetween(1, 28));
+        userBirthDayMonth = faker.options().option("January", "February",
+                "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+        userBirthDayYear = String.valueOf((faker.number().numberBetween(1950, 2000)));
+        userSubject = faker.options().option("Accounting", "Maths", "Arts", "English", "Physics", "Chemistry",
+                "Computer Science", "Economics", "Social Studies", "History", "Civics", "Commerce", "Hindi", "Biology");
+        userHobbies = faker.options().option("Reading", "Sports", "Music");
+        userPicture = "no-homo-sapiens.png";
+        userAddress = faker.address().fullAddress();
+        userState = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
         if (userState == "NCR") {
-        city = faker.options().option("Delhi", "Gurgaon", "Noida");
-    } else if (userState == "Uttar Pradesh") {
-        city = faker.options().option("Agra", "Lucknow", "Merrut");
-    } else if (userState == "Haryana") {
-        city = faker.options().option("Karnal", "Panipat");
-    } else {
-        city = faker.options().option("Jaipur", "Jaiselmer");
+            userCity = faker.options().option("Delhi", "Gurgaon", "Noida");
+        } else if (userState == "Uttar Pradesh") {
+            userCity = faker.options().option("Agra", "Lucknow", "Merrut");
+        } else if (userState == "Haryana") {
+            userCity = faker.options().option("Karnal", "Panipat");
+        } else {
+            userCity = faker.options().option("Jaipur", "Jaiselmer");
+        }
     }
-
-    static String userCity = city;
+    public static Faker faker = new Faker();
+    public String userName;
+    public String lastName;
+    public String userEmail;
+    public String userGender;
+    public String userPhone;
+    public String userBirthDayDay;
+    public String userBirthDayMonth;
+    public String userBirthDayYear;
+    public String userSubject;
+    public String userHobbies;
+    public String userPicture;
+    public String userAddress;
+    public String userState;
+    public String userCity;
 }
